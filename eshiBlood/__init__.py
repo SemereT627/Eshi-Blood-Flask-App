d64d8c4a7d5d8581d1a9b0e9f7d8a8fd76d5b067
Semere Talegngeta <semeretalegn627@gmail.com>
2021-05-20 11:02:26 +0300

    admin and user templates added

--- eshiBlood/__init__.py
+++ eshiBlood/__init__.py
@@ -1,28 +1,20 @@
 import os
+from flask import Flask, render_template
 
-from flask import Flask
+app = Flask(__name__)
 
-def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'test.sqlite'),
-    )
+@app.route('/register')
+def register():
+    return render_template("auth/register.html")
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+@app.route('/login')
+def login():
+    return render_template("auth/login.html")
 
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+@app.route('/admin/index')
+def admin():
+    return render_template("admin/index.html")
 
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
-    return app
+@app.route('/user/index')
+def user():
+    return render_template("user/index.html")
