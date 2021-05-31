5ab37f4b7abd2243281bd03a44e461ddac1b8639
abrham <se.abrham.tesfaye@gmail.com>
2021-05-31 03:17:25 +0300

    added logout method to clear any stored httponly cookie

--- eshiBlood/resources/auth/auth_admin.py
+++ eshiBlood/resources/auth/auth_admin.py
@@ -1,3 +1,5 @@
+import flask
+from flask.json import jsonify
 from flask_restplus import Resource, Namespace
 from eshiBlood.models.models import UserCredential, User, UserRole
 from eshiBlood import bcrypt, db
@@ -25,14 +27,21 @@ class AdminLoginResource(Resource):
                     UserCredential=userCredential.UserCredentialId).first()
             else:
         
-                return {"message": "admin not found"}
+                return {"message": "admin not found"}, 404
 
             role = UserRole.query.filter_by(UserRoleId=user.UserRole).first()
             print(role.RoleName)
             if userCredential and payload['Password'] == userCredential.Password:
                 roleStr = str(role.RoleName).split('.')[-1]
-        
-                return setToken(user.UserId, roleStr), 200
+                response = flask.make_response()
+                response.status_code = 200
+                # response.headers["token"] = setToken(user.UserId, roleStr)
+                
+                tokenValue = setToken(user.UserId, roleStr)
+                print(f"**************token : {tokenValue }")
+                response.set_cookie("token",value = tokenValue,expires=10000000000,httponly=True)
+                
+                return response
             
         return {"message": "Incorrect email or password"}, 400
     
@@ -40,6 +49,15 @@ class AdminLoginResource(Resource):
     def get(self):
         return {"message": "you are logged in"}, 200
 
+# if we want to log out the user we call the unsetToken() function
+# example
+@auth_admin.route("/logout")
+class UserLogOut(Resource):
+    def get(self):
+        response = flask.make_response(jsonify({"message":"you are logged out"}))
+        response.set_cookie("token",value="",expires=0,httponly=True)
+        response.status_code = 200
+        return response
 
 @auth_admin.route('/register')
 class UserRegisterResource(Resource):
